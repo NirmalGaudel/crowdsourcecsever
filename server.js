eec9e0936cd7f0ef3d1caca9bdf2b5dfd7e74a30
Nil Kantha Gaudel <nilkantha.gaudel@palmmind.com>
2020-10-25 17:51:33 +0545

    1

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({
     extended: false
 }));
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use('/assets', express.static('./assets'));
 
 app.use("/", mainRouter);
 app.use("/test", testRouter);
